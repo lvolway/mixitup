1a6aad05dca5b727bad4857fdecf92bf303c6e72
lvolway <59899314+lvolway@users.noreply.github.com>
2020-06-11 18:11:04 -0700

    Add files via upload

--- grc339website1/js/parallax.js
+++ grc339website1/js/parallax.js
@@ -1,19 +1,14 @@
 $(window).scroll(function () {
-    var wScroll = $(this).scrollTop();
-    $(".title").css({
-      transform: "translate(0px, " + wScroll / 2 + "%)"
-    });
+  var wScroll = $(this).scrollTop();
+  $(".title").css({
+    transform: "translate(0px, " + wScroll / 2 + "%)"
   });
-  
-  $(window).scroll(function () {
-    var wScroll = $(this).scrollTop();
-  
-    $(".title").css({
-      transform: "translate(0px, " + wScroll / 1.5 + "%)"
-    });
-  
-    $(".transparent").css({
-      transform: "translate(0px, -" + wScroll / 3 + "%)"
-    });
+});
+
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+
+  $(".title").css({
+    transform: "translate(0px, " + wScroll / 1.5 + "%)"
   });
-  
+});  
